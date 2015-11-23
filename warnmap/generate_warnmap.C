29c10b8e00720b6000ad156cc0c79171f02dbd2c
Nils Feege <nils.feege@stonybrook.edu>
2015-11-23 18:13:43 -0500

    Modify macro to read file ans histogram list from input stering file

--- warnmap/generate_warnmap.C
+++ warnmap/generate_warnmap.C
@@ -1,25 +1,79 @@
+struct dataset{
+  string file;
+  string histogram;
+};
+
 void generate_warnmap()
 {
 
   gSystem->Load("libdppp_warnmap.so");
 
-  int nsigma = 5;
+  int nsigma = 10;
   int niterations = 10;
 
-  //loop over energy ranges
-  for ( int erange = 0; erange < 5; erange++ )
+  string histlist = "generate_warnmap_histlist.txt";
+
+  /* Stream to read table from file */
+  ifstream istream_hist;
+
+  /* Open the file, if it won't open return an error */
+  if (!istream_hist.is_open())
     {
+      istream_hist.open( histlist.c_str() );
+      if(!istream_hist)
+	{
+	  cerr << "ERROR Failed to open file " << histlist << endl;
+	  exit(1);
+	}
+    }
+
+  string line_hist;
+
+  while ( getline( istream_hist, line_hist ) )
+    {
+      /* Skip lines starting with / including a '#' */
+      if ( line_hist.find("#") != string::npos )
+	{
+	  cout << "SKIPPING line in file: " << line_hist << endl;
+	  continue;
+	}
+
+      istringstream iss(line_hist);
+
+      string filename;
+      string histname;
+
+      /* read string- break if error */
+      if ( !( iss >> filename >> histname ) )
+	    {
+	      cerr << "ERROR: Failed to read line in file " << histlist << endl;
+	      exit(1);
+	    }
+
+      /* Build basename for histogram and dataset */
+      string filename_cut = filename;
+      (filename_cut.erase( filename_cut.length()-5 ,5 )).erase(0,25);
+
+      string histname_cut = histname;
+      histname_cut.erase(0,6);
+
+      stringstream basename;
+      basename << filename_cut;
+      basename << histname_cut;
+
+      cout << "Processing: " << filename << " & " << histname << " & " << basename.str() << endl;
+
+      /* Reset ROOT */
       gROOT->Reset();
 
+      /* Name for checkplot file */
       stringstream ss_plotfile;
-      ss_plotfile << "warnmap_output/Checkplots_GernerateWarnmap_nsigma" << nsigma << "_niter" << niterations << "_erange" << erange << ".root";
+      ss_plotfile << "warnmap-output/Checkplots_" << basename.str() << "_nsigma" << nsigma << "_niter" << niterations << ".root";
 
+      /* Run code to generate warnmap */
       direct_photon_pp::GenerateWarnmap *genwarn = new direct_photon_pp::GenerateWarnmap( nsigma, ss_plotfile.str() );
 
-      stringstream ss_histoname;
-      ss_histoname << "hitmap_erange_" << erange;
-
-      int return_code = genwarn->FillHitsFromHistogram( "warnmap_data/WarnmapData_Run13pp510MinBias.root" , ss_histoname.str() );
+      int return_code = genwarn->FillHitsFromHistogram( filename , histname.c_str() );
       if( return_code < 0 )
 	return;
 
@@ -30,12 +84,13 @@ void generate_warnmap()
       genwarn->FiducialCutHotTowers();
       genwarn->FiducialCutSectorEdges();
 
+      /* Write warnmap to text file */
       stringstream ss_warnfile;
-      ss_warnfile << "warnmap_output/Warnmap_Run13pp510MinBias_mergeruns_erange" << erange << ".txt";
+      ss_warnfile << "warnmap-output/Warnmap_" << basename.str() << "_nsigma" << nsigma << "_niter" << niterations << ".txt";
       cout << ss_warnfile.str() << endl;
       genwarn->WriteWarnmap( ss_warnfile.str() );
+
+      delete genwarn;
     }
-
   return;
-
 }
