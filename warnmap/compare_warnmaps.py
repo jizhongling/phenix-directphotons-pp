fbf618aa9c9a8f80fcaa0001e9097294798c8c6c
Nils Feege <nils.feege@stonybrook.edu>
2015-12-04 13:53:53 -0500

    Add file combination

--- warnmap/compare_warnmaps.py
+++ warnmap/compare_warnmaps.py
@@ -20,15 +20,30 @@ plt.rcParams.update({'ytick.labelsize' : 18})
 #            ]
 #plotname = 'plots/warnmap_compare_Run13pp510MinBias_final_erange0.png'
 
-mapfiles = ['warnmap-final/Warnmap_Run13pp510MinBias_Final.txt' ,
-            'warnmap-final/Warnmap_Run13pp510ERT_Final.txt'
-            ]
-plotname = 'plots/warnmap_compare_Run13pp510MinBias_Final_Run13pp510ERT_Final.png'
+#mapfiles = ['warnmap-final/Warnmap_Run13pp510MinBias_Final.txt' ,
+#            'warnmap-final/Warnmap_Run13pp510ERT_Final.txt'
+#            ]
+#plotname = 'plots/warnmap_compare_Run13pp510MinBias_Final_Run13pp510ERT_Final.png'
+
+#mapfiles = [ 'warnmap-output/Warnmap_Run13pp510MinBias_erange_1_nsigma10_niter10.txt' ,
+#             'warnmap-output/Warnmap_Run13pp510MinBias_erange_2_nsigma10_niter10.txt'
+#            ]
+#plotname = 'plots/warnmap_compare_Run13pp510MinBias_erange_1_vs_2.png'
 
 #mapfiles = [ 'warnmap-output/Warnmap_Run13pp510MinBias_erange_0_nsigma10_niter10.txt' ,
 #             'warnmap-output/Warnmap_Run13pp510MinBias_erange_4_nsigma10_niter10.txt'
 #            ]
 #plotname = 'plots/warnmap_compare_Run13pp510MinBias_erange_0_vs_4.png'
+
+#mapfiles = ['warnmap-final/Warnmap_Run13pp510MinBias_Final.txt' ,
+#            'warnmap-final/Warnmap_Run9pp500MinBias_Final.txt'
+#            ]
+#plotname = 'plots/warnmap_compare_Run13pp510MinBias_Run9pp500MinBias.png'
+
+mapfiles = [ 'warnmap-final/Warnmap_Run9pp500MinBias_Final.txt',
+             'warnmap-paul/iter10_rms10/warn_Run9pp500MinBias_newFormat.txt'
+            ]
+plotname = 'plots/warnmap_compare_Run9pp500MinBias_me_vs_paul.png'
 
 #mapfiles = ['warnmap-final/Warnmap_Run13pp510MinBias_Final.txt' ,
 #            'warnmap-paul/iter10_rms10/warn_Run9pp500MinBias.txt'
