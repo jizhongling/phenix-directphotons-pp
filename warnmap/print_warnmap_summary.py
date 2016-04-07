4afe9400ae19f692950885e46e977a10f726811a
Nils Feege <nils.feege@stonybrook.edu>
2016-04-07 15:18:23 -0400

    add printout statements

--- warnmap/print_warnmap_summary.py
+++ warnmap/print_warnmap_summary.py
@@ -2,17 +2,7 @@ import csv
 import numpy as np
 
 mapfiles = [
-    'warnmap-final/Warnmap_Run13pp510MinBias_Final.txt' ,
-    'warnmap-output/Warnmap_Run13pp510MinBias_erange_0_nsigma10_niter10.txt' ,
-    'warnmap-output/Warnmap_Run13pp510MinBias_erange_1_nsigma10_niter10.txt' ,
-    'warnmap-output/Warnmap_Run13pp510MinBias_erange_2_nsigma10_niter10.txt' ,
-    'warnmap-output/Warnmap_Run13pp510MinBias_erange_3_nsigma10_niter10.txt' ,
-    'warnmap-output/Warnmap_Run13pp510MinBias_erange_4_nsigma10_niter10.txt' ,
-    'warnmap-output/Warnmap_Run13pp510MinBias_RunRange1_ERange_erange_0_to_4_nsigma10_niter10.txt' ,
-    'warnmap-output/Warnmap_Run13pp510MinBias_RunRange2_ERange_erange_0_to_4_nsigma10_niter10.txt' ,
-    'warnmap-output/Warnmap_Run13pp510MinBias_RunRange3_ERange_erange_0_to_4_nsigma10_niter10.txt' ,
-    'warnmap-output/Warnmap_Run13pp510MinBias_RunRange4_ERange_erange_0_to_4_nsigma10_niter10.txt' ,
-    'warnmap-output/Warnmap_Run13pp510MinBias_RunRange5_ERange_erange_0_to_4_nsigma10_niter10.txt'
+    'warnmap-final/Warnmap_Run13pp510_Final.txt'
     ]
 
 # array of total towers per sector
@@ -56,7 +46,43 @@ for sector in np.arange(0,8):
         # count hot channels
         nhot = ( ( warnmap[:,0] == sector ) & ( warnmap[:,3] == 50 ) ).sum()
         frachot = float(nhot) / float(ntower_total[sector])
-        print 'sector: %i map: %i hot fraction: %.2f total: %i' % ( sector, i, frachot, nhot )
+        print 'sector: %i map: %i hot fraction: %.2f , count: %i' % ( sector, i, frachot, nhot )
+
+    print ' '
+
+print "*** Number of dead / uncalibrated channels per sector grouped by sector ***"
+for sector in np.arange(0,8):
+
+    for i, warnmap in enumerate( warnmaps ):
+
+        # count bad channels
+        nbad = ( ( warnmap[:,0] == sector ) & ( ( warnmap[:,3] == 100 ) | ( warnmap[:,3] == 150 ) ) ).sum()
+        fracbad = float(nbad) / float(ntower_total[sector])
+        print 'sector: %i map: %i dead / uncalib fraction: %.2f , count: %i' % ( sector, i, fracbad, nbad )
+
+    print ' '
+
+print "*** Number of 'channels around hot/dead/uncalibrated' channels per sector grouped by sector ***"
+for sector in np.arange(0,8):
+
+    for i, warnmap in enumerate( warnmaps ):
+
+        # count excluded channels
+        nbad = ( ( warnmap[:,0] == sector ) & ( warnmap[:,3] == 40 ) ).sum()
+        fracbad = float(nbad) / float(ntower_total[sector])
+        print 'sector: %i map: %i \'excluded around bad\' fraction: %.2f , count: %i' % ( sector, i, fracbad, nbad )
+
+    print ' '
+
+print "*** Number of 'fiducial cut sector edge' channels per sector grouped by sector ***"
+for sector in np.arange(0,8):
+
+    for i, warnmap in enumerate( warnmaps ):
+
+        # count excluded channels
+        nbad = ( ( warnmap[:,0] == sector ) & ( warnmap[:,3] == 20 ) ).sum()
+        fracbad = float(nbad) / float(ntower_total[sector])
+        print 'sector: %i map: %i \'excluded at sector edge\' fraction: %.2f , count: %i' % ( sector, i, fracbad, nbad )
 
     print ' '
 
