d3eac9f45d71d40029c68f7b1ab57f4177d9cc2d
Zhongling Ji <jizhongling@gmail.com>
2018-06-24 20:00:11 -0400

    Change THnSparse to TH1/2/3

---
+++ AnaHistos/anaToFEff_Photon.C
@@ -0,0 +1,82 @@
+#include "GlobalVars.h"
+#include "BBCCounts.h"
+#include "GetEfficiency.h"
+
+void anaToFEff_Photon(const Int_t process = 0)
+{
+  const Int_t secl[2] = {1, 7};
+  const Int_t sech[2] = {6, 8};
+
+  const Int_t nThread = 1000;
+  Int_t thread = -1;
+  Int_t runnumber;
+  ifstream fin("/phenix/plhf/zji/taxi/Run13pp510MinBias/runlist.txt");
+
+  TFile *f_out = new TFile(Form("pileup/ToF-photon-%d.root",process), "RECREATE");
+
+  TGraphAsymmErrors *gr[2];
+  Int_t igp[2] = {};
+  for(Int_t part=0; part<2; part++)
+  {
+    gr[part] = new TGraphAsymmErrors(nThread);
+    gr[part]->SetName(Form("gr_%d",part));
+  }
+
+  ReadClockCounts();
+
+  while( fin >> runnumber )
+  {
+    thread++;
+    if( thread < process*nThread || thread >= (process+1)*nThread ) continue;
+    if( thread%10 == 0 ) cout << "nfile = " << thread << endl;
+
+    TFile *f = new TFile(Form("/phenix/plhf/zji/github/phenix-directphotons-pp/fun4all/offline/analysis/Run13ppDirectPhoton/PhotonNode-macros/histos-ERT/PhotonNode-%d.root",runnumber));
+    if( f->IsZombie() ) continue;
+
+    THnSparse *hn_1photon = (THnSparse*)f->Get("hn_1photon");
+    TAxis *axis_sec = hn_1photon->GetAxis(0);
+    TAxis *axis_pt = hn_1photon->GetAxis(1);
+    TAxis *axis_cut = hn_1photon->GetAxis(3);
+    TAxis *axis_type = hn_1photon->GetAxis(4);
+
+    axis_type->SetRange(3,3);
+    axis_pt->SetRange(26,30);
+
+    ULong64_t nclock = GetClockLive(runnumber);
+    ULong64_t nmb = GetBBCNovtxLive(runnumber);
+
+    for(Int_t part=0; part<2; part++)
+    {
+      axis_sec->SetRange(secl[part],sech[part]);
+      TH1 *h_tof = hn_1photon->Projection(3);
+      Double_t nt = h_tof->GetBinContent(3);
+      Double_t np = h_tof->GetBinContent(4);
+      delete h_tof;
+
+      //Double_t xx = (Double_t)nmb / (Double_t)nclock;
+      Double_t xx = nt;
+      Double_t yy, eyyl, eyyh;
+      if( !GetEfficiency(nt,np, yy,eyyl,eyyh) )
+      {
+        eyyl = yy * sqrt( pow(ent/nt,2.) + pow(enp/np,2.) );
+        eyyh = 0.;
+      }
+      if( yy >= 0. && eyyl >= 0. && eyyl < TMath::Infinity() )
+      {
+        gr[part]->SetPoint(igp[part], xx, yy);
+        gr[part]->SetPointError(igp[part], 0.,0., eyyl,eyyh);
+        igp[part]++;
+      }
+    }
+
+    delete f;
+  }
+
+  f_out->cd();
+  for(Int_t part=0; part<2; part++)
+  {
+    gr[part]->Set(igp[part]);
+    gr[part]->Write();
+  }
+  f_out->Close();
+}
